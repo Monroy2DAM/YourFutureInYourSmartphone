98284a6e558bf46a46640fd6d84b08aeb1a742db
Ismael Núñez <ismael.inc@gmail.com>
2018-02-28 16:25:22 +0100

    Los spinners se cargan mal

---
+++ app/src/main/java/com/erasmusplus/project/yourfutureinyoursmartphone/Handler_Areas.java
@@ -0,0 +1,58 @@
+package com.erasmusplus.project.yourfutureinyoursmartphone;
+
+import java.util.List;
+
+
+public class Handler_Areas {
+
+
+    /**
+     * yf_AREA : [{"ID":"1","NAME":"EDUCATION PRIMARY SCHOOL"},{"ID":"2","NAME":"EDUCATION SECONDARY SCHOOL"},{"ID":"3","NAME":"EDUCATION VOCATIONAL SCHOOL"},{"ID":"4","NAME":"COMPUTER"},{"ID":"5","NAME":"TRANSPORT"}]
+     * success : 1
+     */
+
+    private int success;
+    private List<YfAREABean> yf_AREA;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+
+    public List<YfAREABean> getYf_AREA() {
+        return yf_AREA;
+    }
+
+    public void setYf_AREA(List<YfAREABean> yf_AREA) {
+        this.yf_AREA = yf_AREA;
+    }
+
+    public static class YfAREABean {
+        /**
+         * ID : 1
+         * NAME : EDUCATION PRIMARY SCHOOL
+         */
+
+        private String ID;
+        private String NAME;
+
+        public String getID() {
+            return ID;
+        }
+
+        public void setID(String ID) {
+            this.ID = ID;
+        }
+
+        public String getNAME() {
+            return NAME;
+        }
+
+        public void setNAME(String NAME) {
+            this.NAME = NAME;
+        }
+    }
+}
---
+++ app/src/main/java/com/erasmusplus/project/yourfutureinyoursmartphone/Handler_Ktype.java
@@ -0,0 +1,57 @@
+package com.erasmusplus.project.yourfutureinyoursmartphone;
+
+import java.util.List;
+
+
+public class Handler_Ktype {
+
+    /**
+     * yf_KTYPE : [{"ID":"1","KTYPENAME":"KA1XXX"},{"ID":"2","KTYPENAME":"KA2XXX"}]
+     * success : 1
+     */
+
+    private int success;
+    private List<YfKTYPEBean> yf_KTYPE;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+
+    public List<YfKTYPEBean> getYf_KTYPE() {
+        return yf_KTYPE;
+    }
+
+    public void setYf_KTYPE(List<YfKTYPEBean> yf_KTYPE) {
+        this.yf_KTYPE = yf_KTYPE;
+    }
+
+    public static class YfKTYPEBean {
+        /**
+         * ID : 1
+         * KTYPENAME : KA1XXX
+         */
+
+        private String ID;
+        private String KTYPENAME;
+
+        public String getID() {
+            return ID;
+        }
+
+        public void setID(String ID) {
+            this.ID = ID;
+        }
+
+        public String getKTYPENAME() {
+            return KTYPENAME;
+        }
+
+        public void setKTYPENAME(String KTYPENAME) {
+            this.KTYPENAME = KTYPENAME;
+        }
+    }
+}
